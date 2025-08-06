07e60caae67117fef4c043e6a70263d6df7aef62
Erik <erikeduardorestani@gmail.com>
2025-08-06 19:24:38 -0300

    feat: implementando provider no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:test_fteam/features/characters/data/datasources/character_remote_datasource_impl.dart';
+import 'package:test_fteam/features/characters/domain/repositories/character_repository_impl.dart';
+import 'package:test_fteam/features/characters/domain/usecases/get_characters.dart';
 import 'package:test_fteam/features/characters/presentation/pages/character_list_page.dart';
+import 'package:test_fteam/features/characters/presentation/viewmodels/character_viewmode.dart';
 
 void main() {
   runApp(const RickAndMortyApp());
@@ -10,14 +16,29 @@ class RickAndMortyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rick and Morty',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => CharacterRemoteDatasourceImpl(http.Client())),
+        ProxyProvider<CharacterRemoteDatasourceImpl, CharacterRepositoryImpl>(
+          update: (_, datasource, __) => CharacterRepositoryImpl(datasource),
+        ),
+        ProxyProvider<CharacterRepositoryImpl, GetCharacters>(
+          update: (_, repo, __) => GetCharacters(repo),
+        ),
+        ChangeNotifierProvider<CharacterViewModel>(
+          create: (context) =>
+              CharacterViewModel(context.read<GetCharacters>()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Rick and Morty',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          useMaterial3: true,
+        ),
+        home: const CharacterListPage(),
       ),
-      home: const CharacterListPage(),
     );
   }
 }
