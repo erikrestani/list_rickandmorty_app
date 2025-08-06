b7ba3ebfd46aeccc3d9ad766a20ee865ea8ce1fe
Erik <erikeduardorestani@gmail.com>
2025-08-06 19:22:47 -0300

    feat: criação de repositórios

---
+++ lib/features/characters/domain/repositories/character_repository.dart
@@ -0,0 +1,5 @@
+import 'package:test_fteam/features/characters/domain/entities/character.dart';
+
+abstract class CharacterRepository {
+  Future<List<Character>> getCharacters();
+}
---
+++ lib/features/characters/domain/repositories/character_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:test_fteam/features/characters/domain/entities/character.dart';
+import 'package:test_fteam/features/characters/domain/repositories/character_repository.dart';
+import 'package:test_fteam/features/characters/data/datasources/character_remote_datasource.dart';
+
+class CharacterRepositoryImpl implements CharacterRepository {
+  final CharacterRemoteDatasource datasource;
+
+  CharacterRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Character>> getCharacters() async {
+    final models = await datasource.getCharacters();
+    return models.map((model) => model.toEntity()).toList();
+  }
+}
