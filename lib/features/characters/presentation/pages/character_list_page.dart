3fde2709603d12d0499ebe68853ba7212ee2cde3
Erik <erikeduardorestani@gmail.com>
2025-08-06 19:24:18 -0300

    feat: criação de character list page

--- lib/features/characters/presentation/pages/character_list_page.dart
+++ lib/features/characters/presentation/pages/character_list_page.dart
@@ -1,13 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_fteam/features/characters/presentation/viewmodels/character_viewmode.dart';
+import 'package:test_fteam/features/characters/presentation/widgets/character_card.dart';
 
-class CharacterListPage extends StatelessWidget {
+class CharacterListPage extends StatefulWidget {
   const CharacterListPage({super.key});
 
   @override
+  State<CharacterListPage> createState() => _CharacterListPageState();
+}
+
+class _CharacterListPageState extends State<CharacterListPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        Provider.of<CharacterViewModel>(
+          context,
+          listen: false,
+        ).fetchCharacters();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<CharacterViewModel>();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Rick and Morty')),
-      body: const Center(child: Text('Carregando personagens...')),
+      backgroundColor: const Color(0xFF1A1A1A),
+      appBar: AppBar(
+        title: const Text(
+          'Rick and Morty',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        backgroundColor: const Color(0xFF2C3E50),
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF2C3E50), Color(0xFF1A1A1A)],
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Personagens',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: Builder(
+                builder: (_) {
+                  if (viewModel.isLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  if (viewModel.errorMessage != null) {
+                    return Center(
+                      child: Text(
+                        viewModel.errorMessage!,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                    );
+                  }
+
+                  final characters = viewModel.characters;
+
+                  return ListView.builder(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    itemCount: characters.length,
+                    itemBuilder: (context, index) {
+                      final character = characters[index];
+                      return CharacterCard(
+                        character: character,
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Clicou em ${character.name}'),
+                              backgroundColor: const Color(0xFF27AE60),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
